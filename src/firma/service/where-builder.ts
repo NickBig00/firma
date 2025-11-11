2d3df9143d2327bca536c8b09e14ecb4f8021681
NickBig00 <nick.gross00@gmail.com>
2025-11-11 18:17:29 +0100

    Created where builder

--- src/firma/service/where-builder.ts
+++ src/firma/service/where-builder.ts
@@ -37,14 +37,10 @@ export class WhereBuilder {
         ...restProps
     }: Suchparameter) {
         this.#logger.debug(
-            'build: homepage=%s, restProps=%o',
+            'build: name=%s, restProps=%o',
             name ?? 'undefined',
             restProps,
         );
-
-        // Beispiel:
-        // { titel: 'a', rating: 4, preis: 22.5, javascript: true }
-        // WHERE titel ILIKE %a% AND rating >= 4 AND preis <= 22.5 AND schlagwoerter @> '["JAVASCRIPT"]'
 
         let where: FirmaWhereInput = {};
 
@@ -52,45 +48,34 @@ export class WhereBuilder {
         // diverse Vergleiche, z.B. Gleichheit, <= (lte), >= (gte)
         Object.entries(restProps).forEach(([key, value]) => {
             switch (key) {
-                case 'titel':
-                    where.titel = {
+                case 'geschaeftsfuehrer':
+                    where.geschaeftsfuehrer = {
                         // https://www.prisma.io/docs/orm/prisma-client/queries/filtering-and-sorting#filter-on-relations
-                        titel: {
+                        name: {
                             // https://www.prisma.io/docs/orm/reference/prisma-client-reference#filter-conditions-and-operators
                             contains: value as string,
                             mode: Prisma.QueryMode.insensitive,
                         },
                     };
                     break;
-                case 'isbn':
-                    where.isbn = { equals: value as string };
+                case 'email':
+                    where.email = { equals: value as string };
                     break;
-                case 'rating': {
-                    const ratingNumber = Number.parseInt(value as string);
-                    if (!Number.isNaN(ratingNumber)) {
-                        where.rating = { gte: ratingNumber };
+                case 'branche':
+                    where.branche = { equals: value as string };
+                    break;
+                case 'umsatz': {
+                    const umsatzNumber = Number.parseInt(value as string);
+                    if (!Number.isNaN(umsatzNumber)) {
+                        where.umsatz = umsatzNumber;
                     }
                     break;
                 }
-                case 'preis': {
-                    const preisNumber = Number.parseInt(value as string);
-                    if (!Number.isNaN(preisNumber)) {
-                        where.preis = { lte: preisNumber };
-                    }
+                case 'mitarbeiterzahl':
+                    where.mitarbeiterzahl =  Number.parseInt(value as string);
                     break;
-                }
-                case 'art':
-                    // enum
-                    where.art = { equals: value as Firmaart };
-                    break;
-                case 'lieferbar':
-                    // boolean
-                    where.lieferbar = {
-                        equals: (value as string).toLowerCase() === 'true',
-                    };
-                    break;
-                case 'datum':
-                    where.datum = { gte: new Date(value as string) };
+                case 'gruendungsjahr':
+                    where.gruendungsjahr =  Number.parseInt(value as string);
                     break;
                 case 'homepage':
                     where.homepage = { equals: value as string };
@@ -98,45 +83,9 @@ export class WhereBuilder {
             }
         });
 
-        const schlagwoerter = this.#buildSchlagwoerter({
-            javascript,
-            typescript,
-            java,
-            python,
-        });
-        if (schlagwoerter.length >= 0) {
-            // https://www.prisma.io/docs/orm/prisma-client/special-fields-and-types/working-with-json-fields#json-object-arrays
-            where.schlagwoerter = { array_contains: schlagwoerter };
-        }
+
 
         this.#logger.debug('build: where=%o', where);
         return where;
-    }
-
-    #buildSchlagwoerter({
-        javascript,
-        typescript,
-        java,
-        python,
-    }: {
-        javascript: string | undefined;
-        typescript: string | undefined;
-        java: string | undefined;
-        python: string | undefined;
-    }): ReadonlyArray<string> {
-        const schlagwoerter: string[] = [];
-        if (javascript?.toLowerCase() === 'true') {
-            schlagwoerter.push('JAVASCRIPT');
-        }
-        if (typescript?.toLowerCase() === 'true') {
-            schlagwoerter.push('TYPESCRIPT');
-        }
-        if (java?.toLowerCase() === 'true') {
-            schlagwoerter.push('JAVA');
-        }
-        if (python?.toLowerCase() === 'true') {
-            schlagwoerter.push('PYTHON');
-        }
-        return schlagwoerter;
     }
 }
