46d340c7fdc87db676ccd207e029b1cc8b4b4bde
NickBig00 <nick.gross00@gmail.com>
2025-11-11 17:30:26 +0100

    Added module

--- src/firma/module.ts
+++ src/firma/module.ts
@@ -1,33 +1,17 @@
-// Copyright (C) 2021 - present Juergen Zimmermann, Hochschule Karlsruhe
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 import { Module } from '@nestjs/common';
 import { MailModule } from '../mail/module.js';
 import { KeycloakModule } from '../security/keycloak/module.js';
-import { BuchWriteController } from './controller/buch-write-controller.js';
-import { BuchController } from './controller/firma-controller.ts';
-import { BuchMutationResolver } from './resolver/mutation.js';
-import { BuchQueryResolver } from './resolver/query.js';
-import { BuchService } from './service/buch-service.js';
-import { BuchWriteService } from './service/buch-write-service.js';
+import { FirmaWriteController } from './controller/firma-write-controller.js';
+import { FirmaController } from './controller/firma-controller.ts';
+import { FirmaMutationResolver } from './resolver/mutation.js';
+import { FirmaQueryResolver } from './resolver/query.js';
+import { FirmaService } from './service/firma-service.ts';
+import { FirmaWriteService } from './service/firma-write-service.js';
 import { PrismaService } from './service/prisma-service.js';
 import { WhereBuilder } from './service/where-builder.js';
 
 /**
- * Das Modul besteht aus Controller- und Service-Klassen für die Verwaltung von
- * Bücher.
+ * Firmen.
  * @packageDocumentation
  */
 
@@ -37,17 +21,17 @@ import { WhereBuilder } from './service/where-builder.js';
  */
 @Module({
     imports: [KeycloakModule, MailModule],
-    controllers: [BuchController, BuchWriteController],
+    controllers: [FirmaController, FirmaWriteController],
     // Provider sind z.B. Service-Klassen fuer DI
     providers: [
-        BuchService,
-        BuchWriteService,
-        BuchQueryResolver,
-        BuchMutationResolver,
+        FirmaService,
+        FirmaWriteService,
+        FirmaQueryResolver,
+        FirmaMutationResolver,
         PrismaService,
         WhereBuilder,
     ],
     // Export der Provider fuer DI in anderen Modulen
-    exports: [BuchService, BuchWriteService],
+    exports: [FirmaService, FirmaWriteService],
 })
-export class BuchModule {}
+export class FirmaModule {}
