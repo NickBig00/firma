bce2a94967d8885da023cca1d8cef37773cacf0f
NickBig00 <nick.gross00@gmail.com>
2025-11-12 19:44:46 +0100

    Removed grapql

--- src/module.ts
+++ src/module.ts
@@ -1,17 +1,14 @@
-import { type ApolloDriverConfig } from '@nestjs/apollo';
 import {
     type MiddlewareConsumer,
     Module,
     type NestModule,
 } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
 import { AdminModule } from './admin/module.js';
 import { FirmaWriteController } from './firma/controller/firma-write-controller.js';
-import { FirmaController } from './firma/controller/firma-controller.ts';
+import { FirmaController } from './firma/controller/firma-controller.js';
 import { FirmaModule } from './firma/module.js';
 import { DevModule } from './config/dev/module.js';
-import { graphQlModuleOptions } from './config/graphql.js';
 import { LoggerModule } from './logger/module.js';
 import { RequestLoggerMiddleware } from './logger/request-logger.js';
 import { KeycloakModule } from './security/keycloak/module.js';
@@ -23,7 +20,7 @@ import { KeycloakModule } from './security/keycloak/module.js';
         // Umgebungsvariable DATABASE_URL fuer PrismaPg
         ConfigModule,
         DevModule,
-        GraphQLModule.forRoot<ApolloDriverConfig>(graphQlModuleOptions),
+       // GraphQLModule.forRoot<ApolloDriverConfig>(graphQlModuleOptions),
         LoggerModule,
         KeycloakModule,
     ],
